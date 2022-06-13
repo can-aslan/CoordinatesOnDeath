8e3c3f6e554c1d7fc680347379848eb4199f0614
Yağız Can Aslan <86242974+can-aslan@users.noreply.github.com>
2022-06-13 12:17:15 +0300

    First finalized form of UpdateChecker logic.

--- src/main/java/coordinatesondeath/coordinatesondeath/CoordinatesOnDeath.java
+++ src/main/java/coordinatesondeath/coordinatesondeath/CoordinatesOnDeath.java
@@ -1,6 +1,7 @@
 package coordinatesondeath.coordinatesondeath;
 
 import coordinatesondeath.coordinatesondeath.handlers.DeathHandler;
+import coordinatesondeath.coordinatesondeath.updatecheck.UpdateChecker;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -10,6 +11,13 @@ public final class CoordinatesOnDeath extends JavaPlugin {
     @Override
     public void onEnable() {
         Bukkit.getLogger().info(PREFIX + "CoordinatesOnDeath enabled.");
+        // Update checker logic
+        new UpdateChecker(this).getVersion(version -> {
+            if (this.getDescription().getVersion().equals(version)) getLogger().info("You are running the latest version of CoordinatesOnDeath (" + this.getDescription().getVersion() + ").");
+            else getLogger().info("WARNING: You are running an outdated version of CoordinatesOnDeath (" + this.getDescription().getVersion() + ")! Newest version: " + version + ".");
+        });
+
+        // Plugin logic
         new DeathHandler(this);
     }
 
